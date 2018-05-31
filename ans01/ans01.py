206181dbccb48a3b28fa812f672f0621b4458d6c
Jon Jackson <Jax24135@yahoo.com>
2018-05-31 13:44:36 -0500

    run in Python3

--- ans01/ans01.py
+++ ans01/ans01.py
@@ -1,7 +1,28 @@
+#URL: http://www.pythonchallenge.com/pc/def/map.html
+
 import string
 
 
 def main():
+    phrase = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+    alphabet = list(string.ascii_lowercase)*2
+
+    aList = []
+
+    for word in phrase:
+        for ch in word:
+            if ch.isalnum():
+                #tmpValue = ord(ch) + 2
+                aList.append(chr(ord(ch)+2))
+            else:
+                aList.append(ch)
+
+    #for word in aList:
+        print(aList, end='')
+
+'''
+def main2():
 
     phrase = """g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."""
     
@@ -19,5 +40,6 @@ def main():
             else:
                 print(ch,end='')
     print()
+'''
 
 main()
